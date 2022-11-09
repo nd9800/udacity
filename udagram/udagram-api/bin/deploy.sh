bc9b721c55c05db65807f0badd54ac879cada9f9
Nam Doan <9nam_doan@singaporeair.com.sg>
2022-11-09 23:01:31 +0700

    init

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -2,3 +2,14 @@ eb init udagram-udacity-api --region us-east-1 --platform node.js
 eb create --sample udagram-udacity-api-dev
 eb use udagram-udacity-api-dev
 eb deploy udagram-udacity-api-dev
+eb setenv AWS_BUCKET=$AWS_BUCKET
+eb setenv AWS_PROFILE=$AWS_PROFILE
+eb setenv AWS_REGION=$AWS_REGION
+eb setenv DB_PORT=$DB_PORT
+eb setenv JWT_SECRET=$JWT_SECRET
+eb setenv PORT=$PORT
+eb setenv POSTGRES_DB=$POSTGRES_DB
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
+eb setenv URL=$URL
